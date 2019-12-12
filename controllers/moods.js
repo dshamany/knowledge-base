bcac0757fa296c311f0311eed5892fea70631f8b
dshamany <dshamany@gmail.com>
2019-12-11 17:54:43 -0800

    add moods tracker

--- controllers/moods.js
+++ controllers/moods.js
@@ -1,7 +1,11 @@
 let Mood = require('../models/mood');
 
 module.exports = {
-    index
+    index,
+    create,
+    edit,
+    update,
+    remove
 }
 
 function index(req, res) {
@@ -12,5 +16,39 @@ function index(req, res) {
             moods,
             user: req.user
         });
+    });
+}
+
+function create(req, res) {
+    let mood = new Mood(req.body);
+    mood.save((err) => {
+        if (err) { console.log(err); return; }
+        res.redirect('/moods');
+    });
+}
+
+function edit(req, res) {
+    Mood.findOne({ _id: req.params.id }, (err, mood) => {
+        if (err) { console.log(err); return; }
+        res.render('moods/edit', {
+            title: 'Edit',
+            mood,
+            user: req.user
+        });
+    });
+}
+
+function update(req, res) {
+    Mood.findByIdAndUpdate(req.params.id, req.body, (err, mood) => {
+        if (err) { console.log(err); return; }
+        res.redirect('/moods');
+    });
+}
+
+
+function remove(req, res) {
+    Mood.findByIdAndRemove(req.params.id, req.body, (err, mood) => {
+        if (err) { console.log(err); return; }
+        res.redirect('/moods');
     });
 }
--- routes/moods.js
+++ routes/moods.js
@@ -3,6 +3,10 @@ let router = express.Router();
 let moodCtrl = require('../controllers/moods');
 
 router.get('/', isLoggedIn, moodCtrl.index);
+router.post('/', isLoggedIn, moodCtrl.create);
+router.get('/:id/edit', isLoggedIn, moodCtrl.edit);
+router.put('/:id', isLoggedIn, moodCtrl.update);
+router.delete('/:id', isLoggedIn, moodCtrl.remove);
 
 module.exports = router;
 
